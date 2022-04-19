30c9f9353b7d4100e044bd79748e9592ed48483c
Tim Bryant <timcalvin@gmail.com>
2022-04-18 22:52:56 -0400

    Initial Commit

---
+++ Budge-CD/Budge_CDApp.swift
@@ -0,0 +1,17 @@
+//
+//  Budge_CDApp.swift
+//  Budge-CD
+//
+//  Created by Timothy Bryant on 4/18/22.
+//
+
+import SwiftUI
+
+@main
+struct Budge_CDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
